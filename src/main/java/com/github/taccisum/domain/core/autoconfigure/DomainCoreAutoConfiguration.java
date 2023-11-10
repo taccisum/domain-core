88522b1f00a9d6e0b05550dd4530cb7af72d6265
taccisum <liaojinfeng6937@dingtalk.com>
2023-11-10 16:34:40 +0800

    feat: provide default bean for ExtensibleFactory

--- src/main/java/com/github/taccisum/domain/core/autoconfigure/DomainCoreAutoConfiguration.java
+++ src/main/java/com/github/taccisum/domain/core/autoconfigure/DomainCoreAutoConfiguration.java
@@ -1,14 +1,18 @@
 package com.github.taccisum.domain.core.autoconfigure;
 
+import com.github.taccisum.domain.core.DependenciesManager;
 import com.github.taccisum.domain.core.exception.ErrorCodeMapping;
 import com.github.taccisum.domain.core.exception.ErrorCodeService;
+import com.github.taccisum.domain.core.plugin.ExtensibleFactory;
 import com.github.taccisum.domain.core.spring.FactoryAspect;
 import com.github.taccisum.domain.core.utils.SpringUtils;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.Resource;
 
 /**
  * @author taccisum - liaojinfeng6938@dingtalk.com
@@ -16,7 +20,7 @@ import org.springframework.context.annotation.Import;
  */
 @Import(FactoryAspect.class)
 public class DomainCoreAutoConfiguration implements InitializingBean {
-    @Autowired
+    @Resource
     private ApplicationContext context;
 
     @Bean
@@ -29,6 +33,19 @@ public class DomainCoreAutoConfiguration implements InitializingBean {
         ErrorCodeService bean = new ErrorCodeService("app");
         bean.setErrorCodeMapping(mapping);
         return bean;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ExtensibleFactory extensibleFactory() {
+        ExtensibleFactory factory = new ExtensibleFactory(new DependenciesManager() {
+            @Override
+            public <T> T findOne(Class<T> clazz) {
+                return context.getBean(clazz);
+            }
+        });
+        factory.setApplicationContext(context);
+        return factory;
     }
 
     @Override
