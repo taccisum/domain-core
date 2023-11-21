b7d06bfa6375453ffdfeccecfb7aa90d6dd9fa28
taccisum <liaojinfeng6937@dingtalk.com>
2023-11-21 18:19:19 +0800

    feat: supply KindProperty definition

---
+++ src/main/java/com/github/taccisum/domain/core/data/KindProperty.java
@@ -0,0 +1,19 @@
+package com.github.taccisum.domain.core.data;
+
+/**
+ * Mark the data object has 'kind' property
+ *
+ * @author taccisum - liaojinfeng@baidu.ac.com
+ * @since 2023/11/16
+ */
+public interface KindProperty {
+    /**
+     * @return Kind of its relevant entity
+     */
+    Integer getKind();
+
+    /**
+     * @param kind Kine of its relevant entity
+     */
+    void setKind(Integer kind);
+}
